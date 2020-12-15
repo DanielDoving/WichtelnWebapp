f425ee43404696c8ce9aa138b8e6af99f8f98567
DanielDoving <sillylemon@hotmail.com>
2020-12-15 10:53:34 +0100

    added wishlist page + controller

---
+++ WichtelnWebapp/Server/Controllers/WishListController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WichtelnWebapp.Shared;
+
+namespace WichtelnWebapp.Server.Controllers
+{
+    [ApiController]
+    public class WishListController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetWishList")]
+        public List<WishModel> GetWishList()
+        {
+            List<WishModel> wishlist = new List<WishModel>();
+
+            wishlist.Add(new WishModel { WISH_ID = 1, ITEM = "Lego Starwars", DESCRIPTION = "Millenium Falcon" });
+
+            return wishlist;
+        }
+    }
+}
---
+++ WichtelnWebapp/Shared/WishModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WichtelnWebapp.Shared
+{
+    public class WishModel
+    {
+        public int WISH_ID { get; set; }
+        public String ITEM { get; set; }
+        public String DESCRIPTION { get; set; }
+    }
+}
