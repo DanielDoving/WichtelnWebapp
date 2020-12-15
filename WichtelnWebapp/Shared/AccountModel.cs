a36e17fba47de9403fc5736424eecdf2ea14bbef
DanielDoving <sillylemon@hotmail.com>
2020-12-15 17:40:06 +0100

    Removed unnecessairy files and added login/register

---
+++ WichtelnWebapp/Shared/AccountModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WichtelnWebapp.Shared
+{
+    public class AccountModel
+    {
+        public int ACCOUNT_ID { get; set; }
+
+
+        [Required]
+        [StringLength(50, ErrorMessage = "E-Mail too long")]
+        public string EMAIL { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Username too long")]
+        public string USERNAME { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Name too long")]
+        public string NAME { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Surname too long")]
+        public string SURNAME { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Password too long")]
+        public string PASSWORD { get; set; }
+
+    }
+}
---
+++ WichtelnWebapp/db_structure.sql
@@ -0,0 +1,48 @@
+﻿--
+-- Table structure User
+--
+create table Account(
+	"ACCOUNT_ID"	int identity(1,1),
+	"EMAIL"			varchar(50),
+	"USERNAME"		varchar(20),
+	"NAME"			varchar(20),
+	"SURNAME"		varchar(20),
+	"PASSWORD"		varchar(50)
+	primary key (ACCOUNT_ID)
+);
+
+--
+-- Table structure List
+--
+create table List(
+	"LIST_ID"		int identity(1,1),
+	"FK_ACCOUNT_ID"	int foreign key 
+					references Account(ACCOUNT_ID),
+	primary key (LIST_ID)
+);
+
+--
+-- Table structure Wish
+--
+create table Wish(
+	"WISH_ID"		int identity(1,1),
+	"FK_LIST_ID"	int foreign key 
+					references List(LIST_ID),
+	"ITEM_TITLE"	varchar(256),
+	"ITEM_DESCRIPTION"	text,
+	"GRANTED"		bit default 0,
+	primary key (WISH_ID)
+);
+
+--
+-- Table structure Comment
+--
+create table Comment(
+	"COMMENT_ID"	int identity(1,1),
+	"FK_LIST_ID"	int foreign key 
+					references List(LIST_ID),
+	"FK_ACCOUNT_ID"	int foreign key 
+					references Account(ACCOUNT_ID),
+	"COMMENT"		text
+	primary key (COMMENT_ID)
+);
